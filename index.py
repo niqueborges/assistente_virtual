5de95172c5cb23b79f943e01715f8972a6e4d445
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-05-25 14:27:44 -0300

    🎨 update index.py

--- index.py
+++ index.py
@@ -1,16 +1,44 @@
+# Importa a biblioteca de reconhecimento de voz
 import speech_recognition as sr
 
-mic = sr.Recognizer()
+# Importa a biblioteca de expressões regulares
+import re
 
-with sr.Microphone() as source:
-    mic.adjust_for_ambient_noise(source)
-    
-    print("Please say something:")
-    
-    audio = mic.listen(source)
-    
-    try: 
-        frase = mic.recognize_google(audio, language="pt-BR")
-        print("You said: " + frase)
-    except sr.UnknownValueError:
-        print("Sorry, I could not understand the audio.")
+while(True):
+
+    # Cria um objeto reconhecedor, responsável por processar o áudio
+    mic = sr.Recognizer()
+
+    # Usa o microfone como fonte de entrada de áudio
+    with sr.Microphone() as source:
+        # Ajusta o reconhecimento para ignorar ruídos do ambiente
+        mic.adjust_for_ambient_noise(source)
+        
+        print("Por favor, diga algo:")
+
+        # Escuta o que foi falado no microfone
+        audio = mic.listen(source)
+        
+        try:
+
+            frase = mic.recognize_google(audio,language='pt-BR')
+
+
+
+            if (re.search(r'\b' + "ajudar" + r'\b',format(frase))):
+
+                print("Algo relacionado a ajuda.")
+
+
+
+            elif (re.search(r'\b' + "meu nome é " + r'\b',format(frase))):
+
+                t = re.search('meu nome é (.*)',format(frase))
+
+                nome = t.group(1)
+
+                print("Seu nome é "+nome)
+
+        # Caso não consiga entender o áudio, exibe uma mensagem de erro
+        except sr.UnknownValueError:
+            print("Desculpe, não consegui entender o que você disse.")
