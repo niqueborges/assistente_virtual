6d5c3447796322fb28a60f5f9ce42b1b11122c20
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-05-25 14:31:53 -0300

    ✨ feat: update index.py

--- index.py
+++ index.py
@@ -1,40 +1,87 @@
-# Importa a biblioteca de reconhecimento de voz
 import speech_recognition as sr
 
-# Importa a biblioteca de expressões regulares
 import re
+
+
+
+import pyttsx3 
+
+
+
+nome = ""
+
 
 
 while(True):
 
-    # Cria um objeto reconhecedor, responsável por processar o áudio
+
+
     mic = sr.Recognizer()
 
-    # Usa o microfone como fonte de entrada de áudio
+
+
     with sr.Microphone() as source:
-        # Ajusta o reconhecimento para ignorar ruídos do ambiente
+
+        engine = pyttsx3.init()
+
+        engine.setProperty('voice', "com.apple.speech.synthesis.voice.luciana")
+
         mic.adjust_for_ambient_noise(source)
-        
-        print("Por favor, diga algo:")
 
-        # Escuta o que foi falado no microfone
+
+
+        print("Vamos começar, fale alguma coisa...")
+
+
+
         audio = mic.listen(source)
-        
-        try:
-            frase = mic.recognize_google(audio, language='pt-BR')
 
-            if re.search(r'\bajudar\b', frase):
+
+
+        try:
+
+            frase = mic.recognize_google(audio,language='pt-BR')
+
+
+
+            if (re.search(r'\b' + "ajudar" + r'\b',format(frase))):
+
+
+
+                engine.say("Ajuda");
+
+                engine.runAndWait()
+
                 print("Algo relacionado a ajuda.")
 
-            elif re.search(r'\bmeu nome é\b', frase):
-                t = re.search(r'meu nome é (.*)', frase)
-                if t is not None:
-                    nome = t.group(1)
-                    print("Seu nome é " + nome)
-                else:
-                    print("Não consegui identificar o nome.")
 
-            print("Voce falou: " + frase)
+
+            elif (re.search(r'\b' + "meu nome é " + r'\b',format(frase))):
+
+                t = re.search('meu nome é (.*)',format(frase))
+
+                nome = t.group(1)
+
+                print("Seu nome é "+nome)
+
+                engine.say("Nome falado foi "+nome);
+
+                engine.runAndWait()
+
+
+
+
+
+
+
+            
+
+            print("Voce falou: "+frase)
+
+        
 
         except sr.UnknownValueError:
+
             print("ops, algo deu errado.")
+
+
