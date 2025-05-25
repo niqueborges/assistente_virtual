a54b8f2ee14310414de41513f2f101d90cf7e120
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-05-25 13:56:30 -0300

    ✨ feat: update index.py

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
-print('Olá, mundo!')
+import speech_recognition as sr
+
+mic = sr.Recognizer()
+
+with sr.Microphone() as source:
+    mic.adjust_for_ambient_noise(source)
+    
+    print("Please say something:")
+    
+    audio = mic.listen(source)
+    
+    try: 
+        frase = mic.recognize_google(audio, language="pt-BR")
+        print("You said: " + frase)
+    except sr.UnknownValueError:
+        print("Sorry, I could not understand the audio.")
